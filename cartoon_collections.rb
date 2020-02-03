0257270dc406e710d25c2a694872d7c9ea0b598c
John Perry <jack.e.perry@outlook.com>
2020-02-03 07:25:26 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,34 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |item, index|
+    number = index + 1 
+    puts number.to_s.concat(" #{item}")
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  new_array = []
+  
+  array.map{|item| new_array << "#{item.capitalize}!"}
+  
+  new_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any?{|item| item.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  counter = 0 
+  
+  while counter < cheese_types.length do 
+    if array.include?(cheese_types[counter])
+      return cheese_types[counter]
+    end
+    
+    counter += 1 
+  end
+  
+  nil 
 end
